8eddd96ef2da69b8d5f0a4ddee2fd668e10e8d9e
LyllianT <lilyterrific2008@gmail.com>
2018-07-12 17:46:30 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,21 +4,21 @@
 
 coin_type = {
   :quarters => "0",
-  :dime => "0",
-  :nickel => "0",
-  :penny => "0"
+  :dimes => "0",
+  :nickels => "0",
+  :pennies => "0"
   
 }
 cents = gets.strip
 
 def least_coins(cents)
-  if cents >= 25
+  if cents >= 25 
     :quarters += 1
-    
   elsif cents >= 10
     :dime += 1
   elsif cents >= 5
     :nickel += 1
+  elsif cents >=
   end
 end
 puts least_coins(5)
